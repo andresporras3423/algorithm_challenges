db66ab9265596903934f406300f738ee6f270eb3
andresporras3423 <oscarrussi@outlook.com>
2023-08-22 13:41:41 -0500

    add a third code challengue

---
+++ math_challengues_python/tests/test3.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('Y:\\Desktop\\algorithm_challenges')
+from math_challengues_python import sql_connection
+from functools import reduce
+
+def test3(cursor, conn):
+    data_to_insert = []
+    cursor.execute('''SELECT *
+       FROM [math_challenges].[dbo].[questions_3]''')
+    rows = cursor.fetchall()
+    for row in rows:
+        factorial_n=reduce(lambda x, y: x * y, range(1,int(row.n)+1),1)
+        factorial_n_minus_k=reduce(lambda x, y: x * y, range(1,int(row.n)-int(row.k)+1),1)
+        solution = factorial_n/factorial_n_minus_k
+        data_to_insert.append([row.id,solution])
+    insert_query = "INSERT INTO [math_challenges].[dbo].[solutions_3] values (?, ?)"
+    cursor.executemany(insert_query, [(row[0], row[1]) for row in data_to_insert])
+    conn.commit()
+    try:
+        cursor.execute('EXEC [dbo].[check_question3]')
+        rows = cursor.fetchall()
+        conn.commit()  # Commit the transaction
+        print(f"corrects: {rows[0].corrects}, totals: {rows[0].total}")  
+    except Exception as e:
+        conn.rollback()  # Roll back the transaction
+        print("Error:", e)  
+
+sql_connection.get_con(test3)
