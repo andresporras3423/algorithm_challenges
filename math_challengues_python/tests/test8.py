33dd1c3855d130a325e021da93fe0b9afca8e572
andresporras3423 <oscarrussi@outlook.com>
2023-08-26 08:44:43 -0500

    add challenge 8

---
+++ math_challengues_python/tests/test8.py
@@ -0,0 +1,35 @@
+import sys
+import pyodbc
+sys.path.append('Y:\\Desktop\\algorithm_challenges')
+from math_challengues_python import sql_connection
+from functools import reduce
+import math
+
+def test(cursor, conn):
+    data_to_insert = []
+    cursor.execute('''SELECT *
+       FROM [math_challenges].[dbo].[questions_8]''')
+    rows = cursor.fetchall()
+    for row in rows:
+        average = 0
+        if(row.numbers==''):
+            average='undefined'
+        else:
+            numbers=[float(num) for num in row.numbers.split(',')]
+            average=str(sum(numbers)/len(numbers))
+
+        data_to_insert.append([row.id,average])
+    insert_query = "INSERT INTO [math_challenges].[dbo].[solutions_8] values (?, ?)"
+    # print(data_to_insert)
+    cursor.executemany(insert_query, [(row[0], row[1]) for row in data_to_insert])
+    conn.commit()
+    try:
+        cursor.execute('EXEC [dbo].[check_question8]')
+        rows = cursor.fetchall()
+        conn.commit()  # Commit the transaction
+        print(f"corrects: {rows[0].corrects}, totals: {rows[0].total}") 
+    except Exception as e:
+        conn.rollback()  # Roll back the transaction
+        print("Error:", e)  
+
+sql_connection.get_con(test)
